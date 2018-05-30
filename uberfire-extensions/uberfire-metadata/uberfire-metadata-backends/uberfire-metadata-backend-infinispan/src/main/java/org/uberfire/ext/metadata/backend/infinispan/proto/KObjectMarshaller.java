ee6b818369849c3ec21e753736df9fe8956a56cb
Adriel <adriel.paredes@gmail.com>
2018-05-30 14:43:46 -0300

    [AF-1220]: Infinispan index provider created

---
+++ uberfire-extensions/uberfire-metadata/uberfire-metadata-backends/uberfire-metadata-backend-infinispan/src/main/java/org/uberfire/ext/metadata/backend/infinispan/proto/KObjectMarshaller.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.uberfire.ext.metadata.backend.infinispan.proto;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.infinispan.protostream.MessageMarshaller;
+import org.uberfire.ext.metadata.model.KObject;
+import org.uberfire.ext.metadata.model.KProperty;
+import org.uberfire.ext.metadata.model.impl.KObjectImpl;
+
+public class KObjectMarshaller implements MessageMarshaller<KObject> {
+
+    public static final String SEGMENT_ID = "segmentId";
+    public static final String CLUSTER_ID = "clusterId";
+    public static final String TYPE = "type";
+    public static final String ID = "id";
+    public static final String KEY = "key";
+    public static final String FULL_TEXT = "fullText";
+    private String typeName;
+
+    public KObjectMarshaller(String typeName) {
+
+        this.typeName = typeName;
+    }
+
+    @Override
+    public KObject readFrom(ProtoStreamReader protoStreamReader) throws IOException {
+        String id = protoStreamReader.readString(ID);
+        String type = protoStreamReader.readString(TYPE);
+        String clusterId = protoStreamReader.readString(CLUSTER_ID);
+        String segmentId = protoStreamReader.readString(SEGMENT_ID);
+        String key = protoStreamReader.readString(KEY);
+        List<KProperty<?>> properties = Collections.emptyList();
+        boolean fullText = protoStreamReader.readBoolean(FULL_TEXT);
+
+        return new KObjectImpl(id,
+                               type,
+                               clusterId,
+                               segmentId,
+                               key,
+                               properties,
+                               fullText);
+    }
+
+    @Override
+    public void writeTo(ProtoStreamWriter protoStreamWriter,
+                        KObject kObject) throws IOException {
+        protoStreamWriter.writeString(ID,
+                                      kObject.getId());
+        protoStreamWriter.writeString(TYPE,
+                                      kObject.getType().getName());
+        protoStreamWriter.writeString(CLUSTER_ID,
+                                      kObject.getClusterId());
+        protoStreamWriter.writeString(SEGMENT_ID,
+                                      kObject.getClusterId());
+        protoStreamWriter.writeString(KEY,
+                                      kObject.getKey());
+        protoStreamWriter.writeBoolean(FULL_TEXT,
+                                       kObject.fullText());
+    }
+
+    @Override
+    public Class<? extends KObject> getJavaClass() {
+        return KObject.class;
+    }
+
+    @Override
+    public String getTypeName() {
+        return this.typeName;
+    }
+}
---
+++ uberfire-extensions/uberfire-metadata/uberfire-metadata-backends/uberfire-metadata-backend-infinispan/src/main/java/org/uberfire/ext/metadata/backend/infinispan/provider/InfinispanContext.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.uberfire.ext.metadata.backend.infinispan.provider;
+
+import java.io.IOException;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.impl.ConfigurationProperties;
+import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+import org.uberfire.commons.lifecycle.Disposable;
+import org.uberfire.ext.metadata.backend.infinispan.proto.KObjectMarshallerProvider;
+import org.uberfire.ext.metadata.model.KObject;
+
+public class InfinispanContext implements Disposable {
+
+    private final RemoteCacheManager cacheManager;
+    private final SerializationContext serializationContext;
+
+    public InfinispanContext() {
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.addServer()
+                .host("127.0.0.1")
+                .port(ConfigurationProperties.DEFAULT_HOTROD_PORT)
+                .marshaller(new ProtoStreamMarshaller());
+        cacheManager = new RemoteCacheManager(builder.build());
+
+        serializationContext = ProtoStreamMarshaller.getSerializationContext(cacheManager);
+        serializationContext.registerMarshallerProvider(new KObjectMarshallerProvider());
+    }
+
+    private RemoteCache<String, String> getProtobufCache() {
+        return this.cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+    }
+
+    public RemoteCache<String, KObject> getCache() {
+        return this.cacheManager.getCache();
+    }
+
+    public void addProtobufSchema(String typeName,
+                                  ProtoSchemaBuilder protoSchemaBuilder) {
+
+        try {
+            RemoteCache<String, String> metadataCache = getProtobufCache();
+            metadataCache.put(typeName,
+                              protoSchemaBuilder.build(serializationContext));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        this.cacheManager.stop();
+    }
+}
+
